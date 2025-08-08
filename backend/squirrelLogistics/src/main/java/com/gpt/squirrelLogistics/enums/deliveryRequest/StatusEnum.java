186583f49a1c244e3ded063caefbe4f5590be676
seol <fullstack.eunseolgo14@gmail.com>
2025-08-08 17:02:56 +0900

    feat: 운송 관련 엔티티에대한 상태값 enum 정의

--- backend/squirrelLogistics/src/main/java/com/gpt/squirrelLogistics/enums/deliveryRequest/StatusEnum.java
+++ backend/squirrelLogistics/src/main/java/com/gpt/squirrelLogistics/enums/deliveryRequest/StatusEnum.java
@@ -1,5 +1,10 @@
 package com.gpt.squirrelLogistics.enums.deliveryRequest;
 
 public enum StatusEnum {
-	temporary
+	REGISTERED,  // 공개 게시됨.
+    PROPOSED,    // 특정 운전자에게 제안됨.
+    ASSIGNED,    // 운전자 배차 완료 => DeliveryAssigned로 이관.
+    FAILED,      // 매칭 실패 => 기간 경과.
+    RETRACTED ,  // 요청자가 요청 내림.
+    UNKNOWN		 // 미확인.
 }
--- backend/squirrelLogistics/src/main/java/com/gpt/squirrelLogistics/enums/deliveryWaypoint/StatusEnum.java
+++ backend/squirrelLogistics/src/main/java/com/gpt/squirrelLogistics/enums/deliveryWaypoint/StatusEnum.java
@@ -1,5 +1,10 @@
 package com.gpt.squirrelLogistics.enums.deliveryWaypoint;
 
 public enum StatusEnum {
-	temporary
+	PENDING, // 대기 중. 
+	ARRIVED, // 도착.
+	DROPPED, // 배송 완료.
+	SKIPPED, // 건너뜀.
+	FAILED,  // 사고, 부재중, 잘못된 주소 등 실패.
+	UNKNOWN  // 미확인.
 }
