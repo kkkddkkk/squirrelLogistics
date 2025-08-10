1db92a94ab9c64e4fef043f9086adb3602bdd256
yppal29 <yppal29@naver.com>
2025-08-10 12:51:10 +0900

    cors 에러 해결 추후 리펙토링 필요

--- backend/squirrelLogistics/src/main/java/com/gpt/squirrelLogistics/config/CustomSecurityConfig.java
+++ backend/squirrelLogistics/src/main/java/com/gpt/squirrelLogistics/config/CustomSecurityConfig.java
@@ -4,68 +4,97 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
+/*
+ * API 서버는 외부에서 Ajax 로 호출되기 대문에, 기존의 JSP 서버 방식과는 차이가 많음.
+ * 스프링 Secure 는 기본적으로 다른 도메인에서 Ajax 호출을 차단함.
+ * 때문에 이를 위한 CORS 설정과 GET 방식외의 호출 시에 CSRF 공격을 막기 위한 설정이 기본적으로
+ * 활성화 되어 있으므로 이를 변경해주고 개발 해야함.
+ * 
+ * 이 설정클래스는 이를 적용한 클래스이고, 추가적으로 인증에 관련된 모듈도 추가함.
+ */
 @Configuration
-@EnableWebSecurity
 @Log4j2
 @RequiredArgsConstructor
+//메서드별 권한 설정해보기 : PreAuthorize 를 이용해서 각 경로에 특정한 권한을 가진 사용자만 접근이 가능하도록 제어 할 수 있음
+//여기에 어노테이션을 설정하고, 메서드 선언부에 이를 적용하는 방식을 이용ㅇ함.
+@EnableMethodSecurity
+
 public class CustomSecurityConfig {
+
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-		log.info("=========================== Security Config Start ===========================");
-		httpSecurity.cors(httpSecurityConfigurer -> {
-			httpSecurityConfigurer.configurationSource(corsSource());
+	//위 설정이 적용된 빈을 리턴하는 메서드 정의
+	public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+		log.info("-------------------------- Security Config------------------------");
+		
+		//cors 설정..
+		http.cors(httpSecurityCorsConfigurer ->  {
+			httpSecurityCorsConfigurer.configurationSource(corsSource());
 		});
 		
-		//안전을 위한 세션 기반 인증 무효화 => 서블릿 기반 세션 사용 불가로 막기.
-		httpSecurity.sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		//세션 기반 인증을 무효화 하기 위한 설정 추가..이렇게 하면 서블릿 기반의 세션인증은 사용 불가함.
+		//만약 사용하려면 이걸 삭제하면 됨.
+		http.sessionManagement(sessionConf -> 
+			sessionConf.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 		
-		//GET 이외의 Request 차단, CSRF 토큰 기반의 인증 사용 예정.
-		httpSecurity.csrf(cstf -> cstf.disable());
 		
-		//formLogin: post 방식으로 'username'과 'password'라는 파라미터를 통해 로그인.
-		httpSecurity.formLogin(con -> {
-			con.loginPage("/api/member/login");
-		});
+		//이번엔 GET 방식을 제외하고는 CSRF 토큰(우리가 배울것..) 을 기반으로 인증/인가 처리를 함.
+		//때문에 이 설정도 추가 해야함.
+		http.csrf(csrf -> csrf.disable());
 		
-		return httpSecurity.build();
+		
+		//formLogin 설정을 추가하면, 스프링시큐어는 post 방식으로
+		//username 과 password 라는 파라미터를 통해서 로그인을 처리 할수 있도록 함.
+//		http.formLogin(con -> {
+//			con.loginPage("/api/member/login");
+//			//로그인이 완료되면 등록된 핸들러로 핸들링 하도록 설정
+//			con.successHandler(new APILoginSuccessHandler());
+//			//로긴 실패시 실패 핸들러를 통한 메세지 전송 처리
+//			con.failureHandler(new APILoginFailHandler());
+//		});
+//		
+//		http.addFilterBefore(new JWTCheckerFilter(), UsernamePasswordAuthenticationFilter.class);
+//		
+//		http.exceptionHandling(config -> config.accessDeniedHandler(new CustomAccessDenieHandler()));
+		return http.build();
 	}
 	
 	@Bean
 	public CorsConfigurationSource corsSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-		configuration.setAllowCredentials(true);
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		
-		UrlBasedCorsConfigurationSource  source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
+		corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("HEAD","GET","POST","PUT","DELETE"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization","Cache-Control","Content-Type"));
+		corsConfiguration.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		
+		source.registerCorsConfiguration("/**", corsConfiguration);
 		
 		return source;
 	}
 	
-	//암호를 encoding 빈 => BCryptPas.
+	//암호를 암호화 해주는 빈 등록.
+	//SpringBoot 에서는 BCryptPas..... 라는 빈을 기본적으로 제고 해준다.
+	//얘를 기본 암호화 설정 모듈로 등록 해서 사용한다.
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
 }
+
+
