a349d9510169abde504eded9ff520e85af0d0ab4
yoonjin <skrwns1206@gmail.com>
2025-08-13 15:09:28 +0900

    companyRouter

--- frontend/squirrellogistics/src/routes/company/companyRouter.js
+++ frontend/squirrellogistics/src/routes/company/companyRouter.js
@@ -3,10 +3,16 @@ import { lazy, Suspense } from "react";
 
 const Loading = <div>로딩 중...</div>;
 const MyPage = lazy(() => import("../../pages/company/MyPage"));
-//const MyReviews = lazy(() => import("../../pages/company/MyReviews"));
 const CompanyVerifyPage = lazy(() => import("../../pages/company/CompanyVerifyPage"));
 const CompanyEditPage = lazy(() => import("../../pages/company/CompanyEditPage"));
 const SocialReauthComplete = lazy(() => import("../../pages/company/SocialReauthComplete"));
+const Payment = lazy(() => import("../../pages/Payment/Payment"));
+const Success = lazy(() => import("../../pages/Payment/PaymentSuccess"));
+const History = lazy(() => import("../../pages/History/History"));
+const Report = lazy(() => import("../../pages/Report/Report"));
+const ReportList = lazy(() => import("../../pages/Report/ReportList"));
+const ReviewList = lazy(() => import("../../pages/Review/ReviewList"));
+const ActualCalc = lazy(() => import("../../pages/ActualCalc/ActualCalc"));
 
 const companyRouter = () => {
   return [
@@ -22,14 +28,31 @@ const companyRouter = () => {
       path: "/company/edit",          // ✅ ② 정보수정 (인증 후 진입)
       element: <Suspense fallback={Loading}><CompanyEditPage /></Suspense>,
     },
-    // {
-    //   path: "/company/my-reviews",
-    //   element: <Suspense fallback={Loading}><MyReviews /></Suspense>,
-    // },
     { 
       path: "/company/auth/social-complete",
       element: <Suspense fallback={Loading}><SocialReauthComplete /></Suspense> 
-    },
+    },    {
+      path: "payment",//결제
+      element: <Suspense fallback={Loading}><Payment /></Suspense>,
+    }, {
+      path: `paymentSuccess`,//결제 성공+실패
+      element: <Suspense fallback={Loading}><Success></Success></Suspense>
+    }, {
+      path: `history`,//이용기록
+      element: <Suspense fallback={Loading}><History></History></Suspense>
+    }, {
+      path: `actualCalc`,//실계산
+      element: <Suspense fallback={Loading}><ActualCalc></ActualCalc></Suspense>
+    }, {
+      path: `report`,//신고
+      element: <Suspense fallback={Loading}><Report></Report></Suspense>
+    }, {
+      path: `reportList`,//신고리스트
+      element: <Suspense fallback={Loading}><ReportList></ReportList></Suspense>
+    }, {
+      path: `reviewList`,//리뷰리스트
+      element: <Suspense fallback={Loading}><ReviewList></ReviewList></Suspense>
+    }
   ];
 };
 
