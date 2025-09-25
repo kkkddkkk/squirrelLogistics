8fb5f465ac8336385160bfc96d7b691a2c33ef7f
kindokyeong <kindokyeong@gmail.com>
2025-09-25 18:33:16 +0900

    chore: api 주소 수정

--- frontend/squirrellogistics/src/api/apiServerHost.js
+++ frontend/squirrellogistics/src/api/apiServerHost.js
@@ -1,4 +1,4 @@
-// const API_SERVER_HOST = "https://squirrellogistics.onrender.com/api";
-const API_SERVER_HOST = "http://localhost:8080/api"
+const API_SERVER_HOST = "https://squirrellogistics.onrender.com/api";
+// const API_SERVER_HOST = "http://localhost:8080/api"
 
 export default API_SERVER_HOST;
